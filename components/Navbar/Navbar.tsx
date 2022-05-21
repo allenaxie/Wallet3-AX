9aa4e5533529ea4ed8b3b57eb33be0f16e0e8e79
allenaxie <allenaxie1@gmail.com>
2022-05-21 14:22:29 -0700

    Add user avatar and sub menu to nav bar.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,29 +1,46 @@
 import classes from './Navbar.module.scss';
 import Image from 'next/image';
+import { Avatar, Menu, Dropdown, Button } from 'antd';
 
 const Navbar = () => {
-  return (
-    <div className={classes.container}>
-        <div className={classes.logo}>
-            <Image src="/logo.png" width={60} height={50}/>
-            <span>Wallet3-AX</span>
-        </div>
-        <div>
 
-        </div>
-        <div className={classes.linksUser}>
-            <div className={classes.linksGroup}>
-                <div>Link 1</div>
-                <div>Link 2</div>
-                <div>Link 3</div>
-                <div>Link 4</div>
+    return (
+        <div className={classes.container}>
+            <div className={classes.logo}>
+                <Image src="/logo.png" width={60} height={50} />
+                <span>Wallet3-AX</span>
             </div>
-            <div className={classes.userAvatar}>
-                Avatar
+            <div className={classes.linksUser}>
+                <div className={classes.linksGroup}>
+                    <div>Home</div>
+                    <div>About Us</div>
+                    <div>Link 3</div>
+                    <div>Link 4</div>
+                </div>
+                <div className={classes.userAvatar}>
+                    <Dropdown
+                        overlay={
+                            <Menu className={classes.menu}>
+                                <Menu.Item>
+                                    Profile
+                                </Menu.Item>
+                                <Menu.Item>
+                                    Wallet
+                                </Menu.Item>
+                                <Menu.Item>
+                                    Settings
+                                </Menu.Item>
+                            </Menu>
+                        }
+                        placement="bottomRight"
+                        arrow={true}
+                    >
+                        <Avatar src="https://joeschmoe.io/api/v1/random" />
+                    </Dropdown>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar;
