a7cc7210b02a717b181453c5cf56d30b5182dd7e
allenaxie <allenaxie1@gmail.com>
2022-05-20 23:53:47 -0700

    Create next.js app. Install Sass.

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import classes from './Navbar.module.scss';
+
+const Navbar = () => {
+  return (
+    <div className={classes.container}>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar;
