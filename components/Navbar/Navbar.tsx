1420b3be21b79c1a4331cebc76f5b1cf8297344e
allenaxie <allenaxie1@gmail.com>
2022-05-21 01:00:35 -0700

    Create nav bar layout.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,7 +3,23 @@ import classes from './Navbar.module.scss';
 const Navbar = () => {
   return (
     <div className={classes.container}>
-        Navbar
+        <div className={classes.logo}>
+            LOGO
+        </div>
+        <div>
+
+        </div>
+        <div className={classes.linksUser}>
+            <div className={classes.linksGroup}>
+                <div>Link 1</div>
+                <div>Link 2</div>
+                <div>Link 3</div>
+                <div>Link 4</div>
+            </div>
+            <div className={classes.userAvatar}>
+                Avatar
+            </div>
+        </div>
     </div>
   )
 }
