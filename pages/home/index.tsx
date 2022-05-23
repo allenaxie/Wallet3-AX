dd902235df0e9b8e388875dadc446c105a423fca
allenaxie <allenaxie1@gmail.com>
2022-05-22 22:44:23 -0700

    Create info section.

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -24,6 +24,43 @@ const Home: NextPage = ({ setIndexPage }: any) => {
       </Head>
 
       <Header />
+
+      <Row className={classes.infoSection}>
+        <Col
+          xs={{ span: 24 }}
+          lg={{ span: 8 }}
+        >
+          <Image src="/crypto-wallet.jpeg" width={280} height={160} />
+          <span className={classes.infoWalletTitle}>
+            Connect <span> Wallet </span>
+          </span>
+        </Col>
+        <Col
+          xs={{ span: 24 }}
+          lg={{ span: 8 }}
+        >
+          <Image src="/send-crypto.png" width={280} height={160} />
+          <span className={classes.infoCryptoTitle}>
+            Send <span> Crypto </span>
+          </span>
+        </Col>
+        <Col
+          xs={{ span: 24 }}
+          lg={{ span: 8 }}
+        >
+          <Image src="/track-transactions.png" width={280} height={160} />
+          <span className={classes.infoTxTitle}>
+            Track <span> Transactions </span>
+          </span>
+        </Col>
+        <div className={classes.infoPrimaryText}>
+          <div >
+            Wallet3-AX is your <span> All-in-One </span> Web 3.0 application.
+            <br />
+            <p>More features coming soon.</p>
+          </div>
+        </div>
+      </Row>
       {/* <Footer /> */}
     </div>
   )
